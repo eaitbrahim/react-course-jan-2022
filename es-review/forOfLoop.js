c8f71940ab7282ecab648a00ee0dac91f93495cf
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-10 20:02:30 -0500

    feat(es-review): provide first example of for of loop

---
+++ es-review/forOfLoop.js
@@ -0,0 +1,10 @@
+// Example 1:
+let incomes = [60000, 35000, 75000];
+let total = 0;
+
+for(const income of incomes){
+  console.log(income);
+  total += income;
+}
+
+console.log('Total: ', total);
