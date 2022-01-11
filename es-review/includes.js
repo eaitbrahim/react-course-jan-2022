b68cad14cd9717420789c31872cea62a850d400c
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-10 20:32:59 -0500

    feat(es-review): example of using includes in an array

---
+++ es-review/includes.js
@@ -0,0 +1,7 @@
+let numArray = [1, 2, 3, 4, 5, 6];
+
+// Using indexOf returns the index of the searched value , -1 if it doesn't exist...
+console.log(numArray.indexOf(7));
+
+// Using includes returns true of false
+console.log(numArray.includes(5));
