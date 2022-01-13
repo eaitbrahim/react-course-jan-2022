57afdb01b6eeeeed21c0c3030afa4cde1d2dae59
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-12 19:04:48 -0500

    feat(es-review): Example of using axios to fetch data from jsonplaceholder api

--- es-review/promises.js
+++ es-review/promises.js
@@ -1,12 +1,15 @@
+import axios from 'axios';
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
 function getPosts(){
   // Won't run in node environment, it will run just in the modern browsers or Codepen.io
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(json => console.log('Results as json: ', json));
+  // fetch(apiUrl)
+  //   .then(response => response.json())
+  //   .then(json => console.log('Results as json: ', json));
 
   // Use axios
+  axios.get(apiUrl)
+      .then(response => console.log('response using axios: ', response.data));
 }
 
 getPosts();
