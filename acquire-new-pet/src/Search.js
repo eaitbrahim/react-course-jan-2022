835fda8bbe94d319455bf50abdf965650949d8cf
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-19 20:21:19 -0500

    feat(hooks): Adding select animal

--- acquire-new-pet/src/Search.js
+++ acquire-new-pet/src/Search.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
 const Search = () => {
+  const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
   const [location, setLocation] = useState("Toledo, OH");
-  console.log("Location: ", location);
+  const [animal, setAnimal] = useState("");
+
+  console.log("Animal: ", animal);
 
   return (
     <div>
@@ -16,6 +20,24 @@ const Search = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
