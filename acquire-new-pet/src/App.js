e2bb20a5f98325e6a0250cc2e3e96613dbe6542c
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-19 19:36:04 -0500

    feat(react-from-scratch): Using JSX instead of React createElement

--- acquire-new-pet/src/App.js
+++ acquire-new-pet/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Acquire new Pet"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix"
-    }),
-  ]);
-}
+  return (
+    <div>
+      <h1>Acquire new Pet</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mix" />
+    </div>
+  );
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
