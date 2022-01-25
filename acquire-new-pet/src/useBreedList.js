a0314fdb5700251c2dfaaa890909e033f8453e00
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-24 19:00:40 -0500

    feat(hook): Return operation status from custom hook

--- acquire-new-pet/src/useBreedList.js
+++ acquire-new-pet/src/useBreedList.js
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 
 export default function useBreedList(pet) {
   const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
   const localCache = {};
 
   useEffect(() => {
-    console.log("localCache from use effect: ", localCache);
     if (!pet) {
       setBreedList([]);
     } else if (localCache[pet]) {
@@ -16,15 +16,16 @@ export default function useBreedList(pet) {
 
     async function requestBreedList() {
       setBreedList([]);
+      setStatus("loading");
       const results = await fetch(
         `http://pets-v2.dev-apis.com/breeds?animal=${pet}`
       );
       const json = await results.json();
       localCache[pet] = json.breeds || [];
-      console.log("localCache from requestBreedList: ", localCache);
       setBreedList(localCache[pet]);
+      setStatus("loaded");
     }
   }, [pet]);
 
-  return [breedList];
+  return [breedList, status];
 }
