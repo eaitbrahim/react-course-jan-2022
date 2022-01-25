38d6e49be77199b9c6fd16307a80595ca31be0a9
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-24 21:04:04 -0500

    feat(hooks): Using react router

--- acquire-new-pet/src/App.js
+++ acquire-new-pet/src/App.js
@@ -1,11 +1,22 @@
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./Search";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Acquire new Pet</h1>
-      <Search />
+      <Router>
+        <h1>Acquire new Pet</h1>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Search />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
